c86a4eb1d8d3136936250df0b4aa1dac3d1df8b1
an <740248416@qq.com>
2022-01-16 21:55:32 +0800

    1

--- Class9.cs
+++
@@ -1,8 +0,0 @@
-﻿int IsPass(Maze* m, Position cur, Position next)
-{
-	assert(m != NULL);
-	if (m->_map[next._x][next._y] > m->_map[cur._x][cur._y] || m->_map[next._x][next._y] == 1)
-		return 1;
-
-	return 0;
-}
